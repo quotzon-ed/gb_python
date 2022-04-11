02d5540151aecd4e600ce0920b46741f89a03f32
quotzon <quotzon@gmail.com>
2022-04-11 10:17:18 +0300

    Added some improvements to exercise 4

--- lesson4_5.py
+++ lesson4_5.py
@@ -3,7 +3,13 @@ from functools import reduce
 def f_prod(val_1, val_2):
   return val_1 * val_2
 
-even_list = [val for val in range(100, 1001) if val % 2 == 0]
+# Not cool:
+
+# even_list = [val for val in range(100, 1001) if val % 2 == 0]
+
+# Cool:
+
+even_list = [val for val in range(100, 1001, 2)]
 
 print(even_list)
 print(reduce(f_prod, even_list))
